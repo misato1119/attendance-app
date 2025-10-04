001f45104233bd4fdcb44b2cc32e5369b11baf17
misato <misato@DESKTOP-H7KROUV>
2025-10-04 12:30:08 +0900

    ユーザー情報編集機能追加

--- src/main/java/com/github/misato1119/attendanceapp/controller/UserlistController.java
+++ src/main/java/com/github/misato1119/attendanceapp/controller/UserlistController.java
@@ -6,9 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.github.misato1119.attendanceapp.entity.User;
+import com.github.misato1119.attendanceapp.repository.UserRepository;
 import com.github.misato1119.attendanceapp.service.UserlistService;
+
+
 
 
 @Controller
@@ -16,6 +22,10 @@ public class UserlistController {
 	@Autowired
 	private UserlistService userlistService;
 	
+	@Autowired
+	private UserRepository userRepository;
+	
+	// ユーザー一覧画面表示
 	@GetMapping("/user/list")
 	public String userlist(Model model) {
 		// ユーザーデータ取得
@@ -26,6 +36,26 @@ public class UserlistController {
 		
 		return "user/list";
 	}
+	
+	// ユーザー情報の編集画面表示
+	@GetMapping("/user/edit/{id}")
+	public String useredit(@PathVariable Long id, Model model) {
+		User user = userRepository.findById(id)
+		        .orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + id));
+		model.addAttribute("user", user);
+		return "user/edit";
+	}
+	
+	// ユーザー情報編集画面から申請ボタン押下
+	@PostMapping("/user/update")
+	public String usereditComplete(@ModelAttribute("user") User user) {
+		userlistService.updateUser(user);
+		return "redirect:/user/list";
+	}
+	
+	
+	
+	
 	
 
 }
--- src/main/java/com/github/misato1119/attendanceapp/service/UserlistService.java
+++ src/main/java/com/github/misato1119/attendanceapp/service/UserlistService.java
@@ -1,6 +1,7 @@
 package com.github.misato1119.attendanceapp.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,26 @@ public class UserlistService {
 	// 全件取得
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	// id検索
+	public Optional<User> findById(Long id){
+		return userRepository.findById(id);
+	}
+	
+	// ユーザー情報のアップデート
+	public User updateUser(User user) {
+		// 既存のデータを取得
+		User existing = userRepository.findById(user.getId())
+						.orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + user.getId()));
+		
+		existing.setUsername(user.getUsername());
+		existing.setEmail(user.getEmail());
+		existing.setRole(user.getRole());
+		existing.setDeleted(user.isDeleted());
+		
+		userRepository.save(existing);
+		
+		return existing;
 	}
 }
